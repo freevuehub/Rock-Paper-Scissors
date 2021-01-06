a44ab2b52c3d0dacbdd1b72d17b320a2ab2659ef
doo9392 <hackish@hanmail.net>
2021-01-07 00:49:14 +0900

    [improved]: 로그인창 제작

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,26 @@
 import Head from 'next/head'
+import styled from '@emotion/styled'
+import { Card, Form, Input, Button } from 'antd'
 import { TypeSocket } from '../types'
 
 interface IProps {
   socket: TypeSocket
 }
 
+const MainWrapStyled = styled.div`
+  height: 100%;
+  display: flex;
+  .item {
+    width: 50%;
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+  }
+`
+const CardStyled = styled(Card)`
+  width: 300px;
+`
 const Home: React.FC<IProps> = (props) => {
   props.socket.emit('chat message', 'roomId', 'name', 'message', 'type')
 
@@ -13,9 +29,32 @@ const Home: React.FC<IProps> = (props) => {
       <Head>
         <title>가위바위보 | 메인 페이지</title>
       </Head>
-      <div>
-        Hello World
-      </div>
+      <MainWrapStyled>
+        <div className="item">
+          <CardStyled title="로그인">
+            <Form
+              labelCol={{ span: 5 }}
+              wrapperCol={{ span: 19 }}
+              initialValues={{ remember: true }}
+              name="login"
+            >
+              <Form.Item
+                label="이름"
+                name="name"
+                rules={[{ required: true, message: '이름을 입력해주세요.' }]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item wrapperCol={{ span: 24 }}>
+                <Button block>로그인</Button>
+              </Form.Item>
+            </Form>
+          </CardStyled>
+        </div>
+        <div className="item">
+          Hello World
+        </div>
+      </MainWrapStyled>
     </>
   )
 }
