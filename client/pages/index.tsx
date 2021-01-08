d1e9e39f855922801e8079dbb9b90b97b6bf3c41
doo9392 <hackish@hanmail.net>
2021-01-08 15:27:17 +0900

    [improved]: 방만들기 영역 추가

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styled from '@emotion/styled'
-import { Typography } from 'antd'
+import { Card, Button } from 'antd'
 import { TypeSocket } from '../types'
 
 interface IProps {
@@ -10,13 +10,11 @@ interface IProps {
 const MainWrapStyled = styled.div`
   height: 100%;
   display: flex;
-  .item {
-    width: 50%;
-    display: flex;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-  }
+  align-items: center;
+  justify-content: center;
+`
+const CardStyled = styled(Card)`
+  width: 300px;
 `
 const Home: React.FC<IProps> = (props) => {
   props.socket.emit('chat message', 'roomId', 'name', 'message', 'type')
@@ -27,14 +25,9 @@ const Home: React.FC<IProps> = (props) => {
         <title>가위바위보 | 메인 페이지</title>
       </Head>
       <MainWrapStyled>
-        <div className="item">
-          가위바위보
-        </div>
-        <div className="item">
-          <Typography.Title level={1}>
-            Hello World!
-          </Typography.Title>
-        </div>
+        <CardStyled title="방 만들기">
+          <Button block type="primary" htmlType="submit">만들기</Button>
+        </CardStyled>
       </MainWrapStyled>
     </>
   )
